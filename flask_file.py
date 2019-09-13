74474b08c97ba4b653be90c149d8b694ee20b01e
Aman Goyal <amangoyal1412@gmail.com>
2019-09-13 14:47:39 +0530

    Modified flask

--- flask_file.py
+++ flask_file.py
@@ -1,13 +1,6 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Sep 13 12:07:40 2019
-
-@author: Sairam
-"""
 
 import pandas as pd
-import torch
-import torch.nn as nn
 from flask import Flask,request
 from flask_cors import CORS
 from statsmodels.tsa.arima_model import ARIMA
@@ -23,8 +16,6 @@ def mlwilldoit(time):
     arima_fitted_model = ARIMAResults.load("models/arima_model.pkl")
     diff = monthdelta(datetime.strptime("2017-07-01","%Y-%m-%d"),datetime.strptime(time,"%Y-%m-%d"))
     return str(arima_fitted_model.forecast(diff)[0][-1])
-def ml_forecast_this():
-    pass
 
 def monthdelta(d1, d2):
     delta = 0
@@ -41,10 +32,7 @@ def monthdelta(d1, d2):
 def current_value(time):
     return mlwilldoit(time)
 
-@app.route("/forecast")
-def forecast():
-    data=request.data['id']
-    return ml_forecast_this()
-
-
-
+	
+	
+if __name__=="__main__":
+	app.run()
